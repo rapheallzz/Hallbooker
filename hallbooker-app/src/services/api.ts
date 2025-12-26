0935e43539beed92575d4cf509358dbc89d30a34
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2025-12-26 13:36:23 +0100

    update api

--- hallbooker-app/src/services/api.ts
+++ hallbooker-app/src/services/api.ts
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://hallbooker.onrender.com/api/v1',
+  baseURL: 'https://qspfh08j-8000.uks1.devtunnels.ms/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+// https://hallbooker.onrender.com/api/v1
+// https://qspfh08j-8000.uks1.devtunnels.ms/api/v1
 
 api.interceptors.request.use(
   (config) => {
