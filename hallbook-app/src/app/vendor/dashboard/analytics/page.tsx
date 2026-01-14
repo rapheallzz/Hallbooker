f619bc6de6f391e0f404004d72ca7ecb8fa55ac9
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2026-01-14 13:32:02 +0100

    delete

--- hallbook-app/src/app/vendor/dashboard/analytics/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from 'react';
-import EnhancedAnalytics from '@/components/vendor/EnhancedAnalytics';
-
-const AnalyticsPage = () => {
-  return <EnhancedAnalytics />;
-};
-
-export default AnalyticsPage;
